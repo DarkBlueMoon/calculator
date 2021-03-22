080183d6e1dcc2db44a8b27994598387831c91d4
Demetri Evans <darkbluemoon93@gmail.com>
2021-03-21 21:53:41 -0500

    Initial commit. Added calculating functions. Added HTML buttons.

---
+++ script.js
@@ -0,0 +1,33 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  if (b === 0) {
+    return "Cannot divide by zero.";
+  }
+  return a / b;
+};
+
+const operate = function (num1, num2, op) {
+  switch (op) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      return "Invalid operator. Valid operators are '+', '-', '*', '/'";
+  }
+};
