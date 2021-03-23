95ef894523fd0b707a3e9c89e883ceec6a132538
Demetri Evans <darkbluemoon93@gmail.com>
2021-03-23 07:09:09 -0500

    Worked out storing current number in variable. Still planning the rest of functionality.

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const wrapper = document.querySelector("#button-wrapper");
 const display = document.querySelector("#display");
+let preOperValue = "";
+let postOperValue = "";
+let currentOp = "";
+let totalVal = 0;
+
+/*
+Two vars to store the #s, one var to store the operator, one var to store the result.
+  The operator isn't used until the second number comes in.
+  User enters num1, that's stored. User presses operator, that's stored.
+  User enters num2, two outcomes:
+    User presses operator: 1 + 2 +
+      Store #1 (op) #2 in first variable: 3
+      Clear (op) and #2: "", ""
+      Update display w/ first variable: 3
+      User presses num then op again: 4 +
+        Store #1 (op) #2 in first var: 7
+        Clear (op) and #2: "", ""
+        Update display w/ first var: 7
+    User presses equals: 1 + 2 =
+      Store #1 (op) #2 in first variable: 3
+      Clear (op) and #2: "", ""
+      Update display w/ first var: 3
+      End this chain of calculations: clear var1 (?)
+*/
 
 wrapper.addEventListener("click", (e) => {
   const isButton = e.target.nodeName === "BUTTON";
@@ -8,9 +32,24 @@ wrapper.addEventListener("click", (e) => {
   }
 
   if (e.target.classList.contains("number")) {
-    // console.log(e);
-    display.value += e.target.innerText;
+    preOperValue += e.target.innerText;
+    display.value = preOperValue;
+  } else {
+    currentOp = e.target.innerText;
   }
+
+  // if (e.target.classList.contains("operator")) {
+  //   // If operator is empty, we are calculating for the first time. Wait for second number.
+  //   if (currentOp === "") {
+
+  //   } else {
+
+  //   }
+  //   // totalVal += parseInt(display.value) || 0;
+  //   // totalVal += operate(totalVal, parseInt(display.value), e.target.innerText);
+  //   // display.value = "";
+  //   // console.log(totalVal);
+  // }
 });
 
 const add = function (a, b) {
