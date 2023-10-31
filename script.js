af5cd4d4df446ea767f3084bbbd37031775f0408
Demetri Evans <3819574+DarkBlueMoon@users.noreply.github.com>
2023-10-31 17:40:36 -0500

    Calculate with clicking operator buttons

--- script.js
+++ script.js
@@ -1,6 +1,72 @@
+const btnContainer = document.querySelector("#button-container");
+const numberDisplay = document.querySelector("#display");
+
 let leftOperand = 0;
-let operator = '';
+let operator = "";
 let rightOperand = 0;
+
+let displayValue = "";
+let clearDisplayFlag = false;
+let resetFlag = false;
+
+btnContainer.addEventListener("mousedown", (e) => {
+  const target = e.target;
+
+  if (target.classList.contains("number")) {
+    if (clearDisplayFlag) {
+      numberDisplay.value = "";
+      clearDisplayFlag = false;
+    }
+
+    numberDisplay.value += target.textContent;
+  } else if (target.classList.contains("operator")) {
+
+    if (checkForCalculation()) {
+      let result = calculateResult(leftOperand, operator);
+      updateDisplay(result);
+      // rightOperand = +numberDisplay.value;
+      // numberDisplay.value = operate(leftOperand, operator, rightOperand);
+      // leftOperand = numberDisplay.value;
+      // operator = "";
+      // rightOperand = 0;
+    }
+
+    clearDisplayFlag = true;
+
+    leftOperand = +numberDisplay.value;
+    operator = target.textContent;
+
+  } else if (target.classList.contains("equals")) {
+    if (leftOperand && operator) {
+      rightOperand = +numberDisplay.value;
+      numberDisplay.value = operate(leftOperand, operator, rightOperand);
+      leftOperand = numberDisplay.value;
+      operator = "";
+      rightOperand = 0;
+    }
+    resetFlag = true;
+  }
+});
+
+function checkForCalculation() {
+  return leftOperand && operator;
+}
+
+function updateDisplay(res) {
+  numberDisplay.value = res;
+  leftOperand = numberDisplay.value;
+  operator = "";
+  rightOperand = 0;
+}
+
+function calculateResult(leftOperand, operator) {
+  rightOperand = +numberDisplay.value;
+  return operate(leftOperand, operator, rightOperand);
+  // updateDisplay(result);
+  // leftOperand = numberDisplay.value;
+  // operator = "";
+  // rightOperand = 0;
+}
 
 function operate(num1, op, num2) {
   switch (op) {
